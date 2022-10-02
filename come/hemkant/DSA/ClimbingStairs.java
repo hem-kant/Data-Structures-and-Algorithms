1cdec6182263ab28839dcc599b65520e984b1f0c
Hem Kant <hemkant.india@gmail.com>
2022-10-02 10:13:46 +0530

    Initial Commit
    Initial Commit

---
+++ come/hemkant/DSA/ClimbingStairs.java
@@ -0,0 +1,19 @@
+package come.hemkant.DSA;
+
+public class ClimbingStairs {
+
+    public static void main(String args[])
+    {
+        int temp=0;
+        int range =5;
+        int num1 =1, num2=1;
+        for (int i=0;i<(range-1);i++)
+        {
+            temp=num1;
+            num1=num1+num2;
+            num2=temp;
+            System.out.println(num2);
+        }
+        System.out.println("Number of ways = " + num1);
+    }
+}
