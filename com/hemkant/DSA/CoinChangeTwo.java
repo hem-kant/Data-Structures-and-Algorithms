5dcf202db2d84e161080649682234f0c7e6b93e8
Hem Kant <hemkant.india@gmail.com>
2022-10-05 16:21:40 +0530

    Coin Change Challenge 2

---
+++ com/hemkant/DSA/CoinChangeTwo.java
@@ -0,0 +1,24 @@
+package com.hemkant.DSA;
+
+public class CoinChangeTwo {
+
+    static long getNumberOfWays(int amount, int[] coins)
+    {
+        int[] numberOfWays = new int [amount + 1];
+        numberOfWays[0] = 1;
+        for (int i = 0; i < coins.length; i++) {
+
+            for (int j = 0; j < numberOfWays.length; j++) {
+                if (coins[i] <= j) {
+                    numberOfWays[j] += numberOfWays[(int)(j - coins[i])];
+                }
+            }
+        }
+        return numberOfWays[amount];
+    }
+    public static void main(String[] args)
+    {
+        int coins[] = { 1, 5, 10 };
+        System.out.println(getNumberOfWays(8, coins));
+    }
+}
